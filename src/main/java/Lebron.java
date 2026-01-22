b3a1f7d78a10b19e3a78784c2deaf87baaa71000
Reiner Ong <e1337575@u.nus.edu>
2026-01-23 02:16:53 +0800

    Lebron.java: Echo commands and exit on bye

--- src/main/java/Lebron.java
+++ src/main/java/Lebron.java
@@ -1,11 +1,33 @@
+import java.util.Scanner;
+
 public class Lebron {
     public static void main(String[] args) {
         System.out.println("---------------------------");
         System.out.println("Hello! I'm Lebron James");
         System.out.println("What can I do for you?");
         System.out.println("---------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("---------------------------");
+        
+        Scanner scanner = new Scanner(System.in);
 
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            respondToInput(input);
+            if (input.equals("bye")) {
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static void respondToInput(String input) {
+        System.out.println("---------------------------");
+        if (input.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        } else {
+            System.out.println(input);
+        }
+        System.out.println("---------------------------");
     }
 }
